77e2e377c790f0007e9ef2e3ff42bfba5af2b8b7
Johannes Kjaer <77483678+johannes-kjaer@users.noreply.github.com>
2023-12-20 16:58:54 +0100

    Got the model to work with multiple classes

--- ShapeDataset.py
+++ ShapeDataset.py
@@ -1,9 +1,17 @@
+from typing import Any, Tuple
 import numpy as np
 import os
 from PIL import Image
 
 import torch
+import torchvision
 from torchvision import transforms as T
+
+
+import os
+import torch.utils.data
+from pycocotools.coco import COCO
+
 
 
 class ShapeDataset(torch.utils.data.Dataset):
@@ -44,3 +52,146 @@ class ShapeDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.imgs)
+    
+class ShapeDataset2(torchvision.datasets.CocoDetection):
+    def __init__(self, root, feature_extractor, train=True):
+        ann_file = os.path.join(root, "_annotations.coco.json")
+        super(ShapeDataset2, self).__init__(root, ann_file)
+        self.feature_extractor = feature_extractor
+
+        self.coco = COCO(ann_file)
+        self.ids = list(sorted(self.coco.imgs.keys()))
+
+    def __getitem__(self, idx):
+        # read in PIL image and target in COCO format
+        img, target = super(ShapeDataset2, self).__getitem__(idx)
+
+        # preprocess image and target (converting target to DETR format, resizing + normalization of both image and target)
+        image_id = self.ids[idx]
+        target = {'image_id': image_id, 'annotations': target}
+        #print(target["annotations"][0]["bbox"])
+
+        img_id = self.ids[idx]
+        ann_ids = self.coco.getAnnIds(imgIds=img_id)
+        coco_annotation = self.coco.loadAnns(ann_ids)
+        num_objs = len(coco_annotation)
+        boxes = []
+        for i in range(num_objs):
+            xmin = coco_annotation[i]['bbox'][0]
+            ymin = coco_annotation[i]['bbox'][1]
+            xmax = xmin + coco_annotation[i]['bbox'][2]
+            ymax = ymin + coco_annotation[i]['bbox'][3]
+            boxes.append([xmin, ymin, xmax, ymax])
+        #boxes = torch.as_tensor(boxes, dtype=torch.float32)
+
+        target["annotations"][0]["bbox"] = boxes
+
+        encoding = self.feature_extractor(images=img, annotations=target, return_tensors="pt")
+        pixel_values = encoding["pixel_values"].squeeze() # remove batch dimension
+        target = encoding["labels"][0] # remove batch dimension
+
+        return pixel_values, target
+
+
+
+
+
+
+
+
+
+
+
+
+class ShapeDataset3(torch.utils.data.Dataset):
+    def __init__(self, root, annotation, transforms=None):
+        self.root = root
+        self.transforms = transforms
+        self.coco = COCO(os.path.join(root,annotation))
+        self.ids = list(sorted(self.coco.imgs.keys()))
+
+    def __getitem__(self, index):
+        # Image ID
+        img_id = self.ids[index]
+        # List: get annotation id from coco
+        ann_ids = self.coco.getAnnIds(imgIds=img_id)
+        # Dictionary: target coco_annotation file for an image
+        coco_annotation = self.coco.loadAnns(ann_ids)
+        # path for input image
+        path = self.coco.loadImgs(img_id)[0]['file_name']
+        # open the input image
+        img = Image.open(os.path.join(self.root, path)).convert('RGB')
+        image = torchvision.tv_tensors.Image(img)
+
+        # number of objects in the image
+        num_objs = len(coco_annotation)
+        #print(coco_annotation)
+        
+        # Bounding boxes for objects
+        # In coco format, bbox = [xmin, ymin, width, height]
+        # In pytorch, the input should be [xmin, ymin, xmax, ymax]
+        boxes = []
+        for i in range(num_objs):
+            xmin = coco_annotation[i]['bbox'][0]
+            ymin = coco_annotation[i]['bbox'][1]
+            xmax = xmin + coco_annotation[i]['bbox'][2]
+            ymax = ymin + coco_annotation[i]['bbox'][3]
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        boxes = torch.as_tensor(boxes)
+
+        # Labels
+        #labels = torch.ones((num_objs,), dtype=torch.int64)
+        labels = np.zeros(num_objs,dtype=np.int64)
+        for i in range(num_objs):
+            labels[i] = coco_annotation[i]["category_id"]
+        labels = torch.as_tensor(labels)        
+
+        # Tensorise img_id
+        img_id = torch.tensor([img_id])
+        # Size of bbox (Rectangular)
+        areas = []
+        for i in range(num_objs):
+            areas.append(coco_annotation[i]['area'])
+        areas = torch.as_tensor(areas, dtype=torch.float32)
+        # Iscrowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+        
+        # Segmentation
+        masks = []
+        for seg in coco_annotation:
+            masks.append(self.coco.annToMask(seg))
+        masks = torch.as_tensor(masks)
+
+        # Annotation is in dictionary format
+        my_annotation = {}
+        my_annotation["boxes"] = boxes      # I think this one is good
+        my_annotation["labels"] = labels
+        my_annotation["image_id"] = index
+        my_annotation["area"] = areas
+        my_annotation["iscrowd"] = iscrowd
+        my_annotation["masks"] = masks     # I think this one is good
+
+
+        # Debugging prints
+        #print(my_annotation)
+
+        if self.transforms is not None:
+            image = self.transforms(image)
+
+        return (image, my_annotation)
+
+    def __len__(self):
+        return len(self.ids)
+    
+# ,transform=torchvision.transforms.PILToTensor
+
+class CustCoco(torchvision.datasets.CocoDetection):
+    def __init__(self,root, ann_file, transforms=None):
+        super().__init__(root, ann_file, transforms)
+    
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        image, target = super().__getitem__(index)
+        img = torchvision.transforms.PILToTensor()(image)
+        return img, target
+
--- implementations.py
+++ implementations.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch
 from ShapeDataset import *
+import torchvision
+from torchvision.transforms import v2
 
 
 def custom_collate(data):
@@ -27,6 +29,41 @@ def getTrainVal(images, masks):
 def get_dataloader(images , masks, root, set_num_workers):
     return torch.utils.data.DataLoader(
         ShapeDataset(images , masks, root) ,
+        batch_size = 2 ,
+        shuffle = True ,
+        collate_fn = custom_collate,
+        num_workers = set_num_workers ,
+        pin_memory = True if torch.cuda.is_available() else False
+    )
+
+def get_dataloader2(root, feature_extractor, set_num_workers):
+    return torch.utils.data.DataLoader(
+        ShapeDataset2(root, feature_extractor) ,
+        batch_size = 2 ,
+        shuffle = True ,
+        collate_fn = custom_collate,
+        num_workers = set_num_workers ,
+        pin_memory = True if torch.cuda.is_available() else False
+    )
+
+
+def get_transform():
+    custom_transforms = []
+    custom_transforms.append(torchvision.transforms.ToTensor())
+    return torchvision.transforms.Compose(custom_transforms)
+
+def get_transform3(train):
+    transforms = []
+    if train:
+        transforms.append(v2.RandomHorizontalFlip(0.5))
+    transforms.append(v2.ToDtype(torch.float, scale=True))
+    transforms.append(v2.ToPureTensor())
+    return v2.Compose(transforms)
+
+
+def get_dataloader3(root, set_num_workers):
+    return torch.utils.data.DataLoader(
+        ShapeDataset3(root, '_annotations.coco.json' , transforms=get_transform3(train = True)) ,
         batch_size = 2 ,
         shuffle = True ,
         collate_fn = custom_collate,
